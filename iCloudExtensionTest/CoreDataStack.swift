410f91a7a940b9d3c339303e2d2704f501774d2f
Arno Appenzeller <arno.appenzeller@gmx.de>
2014-09-12 20:19:20 +0200

    Extension Working on with CoreData but without iCloud

---
+++ iCloudExtensionTest/CoreDataStack.swift
@@ -0,0 +1,66 @@
+//
+//  File.swift
+//  iCloudExtensionTest
+//
+//  Created by Arno Appenzeller on 12.09.14.
+//  Copyright (c) 2014 Arno. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack{
+    
+    init(docURL: NSURL,modelURL: NSURL) {
+        self.applicationDocumentsDirectory = docURL
+        self.managedObjectModelURL = modelURL
+    }
+    
+    
+    var applicationDocumentsDirectory : NSURL
+    
+    var managedObjectModelURL: NSURL
+    
+    lazy var managedObjectModel: NSManagedObjectModel = {
+        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
+        return NSManagedObjectModel(contentsOfURL: self.managedObjectModelURL)
+        }()
+    
+    
+     lazy private var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
+        // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
+        // Create the coordinator and store
+        var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        var error: NSError? = nil
+        //let theUrl = self.applicationDocumentsDirectory.URLByAppendingPathComponent("iCloudExtensionTest.sqlite")
+        var failureReason = "There was an error creating or loading the application's saved data."
+        if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: self.applicationDocumentsDirectory, options: nil, error: &error) == nil {
+            coordinator = nil
+            // Report any error we got.
+            let dict = NSMutableDictionary()
+            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
+            dict[NSLocalizedFailureReasonErrorKey] = failureReason
+            dict[NSUnderlyingErrorKey] = error
+            error = NSError.errorWithDomain("YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+            // Replace this with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog("Unresolved error \(error), \(error!.userInfo)")
+            abort()
+        }
+        
+        return coordinator
+        }()
+    
+    lazy var managedObjectContext: NSManagedObjectContext? = {
+        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
+        let coordinator = self.persistentStoreCoordinator
+        if coordinator == nil {
+            return nil
+        }
+        var managedObjectContext = NSManagedObjectContext()
+        managedObjectContext.persistentStoreCoordinator = coordinator
+        return managedObjectContext
+        }()
+    
+}
+
